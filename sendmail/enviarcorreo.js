bfb7c5463d61471899506feae781b485409aa31d
Julio Solis <js@juliosolis.com>
2019-10-22 17:23:25 -0600

    Initialize Web Site

---
+++ sendmail/enviarcorreo.js
@@ -0,0 +1,45 @@
+$(function() {
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Form
+	/* ---------------------------------------------------------------------- */
+
+	var submitContact = $('#formulario-contacto, #contacto-footer');
+		//message = $('.mensaje');
+
+	submitContact.on('submit', function(e){
+		e.preventDefault();
+
+		var $this = $(this);
+		idioma = $this.children("#idioma").val();
+		message = $this.children('#mensaje');
+
+		//idioma = $("#idioma").val();
+		texto_1 = 'Enviar';
+		texto_2 = 'Enviando...';
+		if(idioma !== 'es'){
+			texto_1 = 'Send';
+			texto_2 = 'Sending...';
+		}
+
+		$this.children(".boton").attr('disabled', 'disabled');
+		$this.children(".boton").val(texto_2);
+
+		$.ajax({
+			type: "POST",
+			url: '../sendmail/enviarmensaje.php',
+			dataType: 'json',
+			cache: false,
+			data: $this.serialize(),
+			success: function(data) {
+				$this.children(".boton").removeAttr('disabled');
+				$this.children(".boton").val(texto_1);
+				if(data.info !== 'error'){
+					$this.children(".reset").val('');
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				} else {
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-danger').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');					
+				}
+			}
+		});
+	});
+});
