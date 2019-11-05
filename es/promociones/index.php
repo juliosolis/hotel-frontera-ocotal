cb7dd4ebc7140fdedd4c3ac9ff69a0b151907bff
Julio Solis <js@juliosolis.com>
2019-11-05 10:34:48 -0600

    Google Analytics for new promociones/index.php

--- es/promociones/index.php
+++ es/promociones/index.php
@@ -8,6 +8,15 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js"> <!--<![endif]-->
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151434276-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-151434276-1');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Promociones | Hotel Frontera en Ocotal Nicaragua</title>
